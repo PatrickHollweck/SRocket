7194ded2136c62f7332ea48a9d8d7e5e1395de1a
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-02-12 22:17:49 +0100

    Refactor and reimplemented Typedpair since it got deleted somehow

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -2,18 +2,24 @@ import * as _ from 'lodash';
 import * as RouteDecorator from './decorator/Route';
 
 import { RouteConfig } from './RouteConfig';
-import Route from './Route';
 import { TypedPair } from '../structures/Pair';
+import Route from './Route';
 
 const debug = require('debug')('srocket:Router');
 
 export type NewableRoute = { new(...args: any[]): Route };
 
+export enum RouterCallbackType {
+	BEFORE_EVENT,
+	ON_EVENT,
+	AFTER_EVENT,
+}
+
 class InternalRoute extends Route {
 	public config: RouteConfig;
 	public instance: Route;
 
-	public constructor(config:RouteConfig, instance:Route) {
+	public constructor(config: RouteConfig, instance: Route) {
 		super();
 
 		this.config = config;
@@ -29,25 +35,33 @@ class InternalRoute extends Route {
 	}
 }
 
+// TODO: Implement placeholder fields.
+
 export default class Router {
-	private routes:InternalRoute[];
+	private routes: InternalRoute[];
+	private beforeCallbacks: Function[];
+	private onCallbacks: Function[];
+	private afterCallbacks: Function[];
 
 	public constructor() {
 		this.routes = new Array<InternalRoute>();
+
+		this.beforeCallbacks = new Array<Function>();
+		this.onCallbacks = new Array<Function>();
+		this.afterCallbacks = new Array<Function>();
 	}
 
 	public route(packet: SocketIOExt.Packet, socket: SocketIOExt.Socket) {
-		for (const route of this.routes) {
-			if (route.getRoute() === packet.data[0]) {
-				this.invokeRoute(route, socket);
-				return;
-			}
+		const route = this.findRoute(packet);
+		if(!route) {
+			debug(`WARNING! - Could not find a route for ${packet.data[0]}`);
+			return;
 		}
 
-		debug(`WARNING! - Could not find a route for ${packet.data[0]}`);
+		this.invokeRoute(route, socket);
 	}
 
-	public register(route: NewableRoute, routeConfig?:RouteConfig) {
+	public register(route: NewableRoute, routeConfig?: RouteConfig) {
 		const instance = new route();
 		const internalRoute = new InternalRoute(routeConfig || this.getRouteConfig(route), instance);
 
@@ -55,16 +69,16 @@ export default class Router {
 
 		const nestedRoutes = new Array<TypedPair<RouteConfig, NewableRoute>>();
 		const properties = Object.getOwnPropertyNames(instance);
-		for(const property of properties) {
+		for (const property of properties) {
 			const metadata = RouteDecorator.getNestedRouteMetadata(instance, property);
-			if(metadata) {
+			if (metadata) {
 				const nestedRoute = instance[property];
 				nestedRoutes.push(new TypedPair(metadata, nestedRoute));
 			}
 		}
 
-		if(nestedRoutes.length > 0) {
-			for(const nestedRoute of nestedRoutes) {
+		if (nestedRoutes.length > 0) {
+			for (const nestedRoute of nestedRoutes) {
 				nestedRoute.key.route = internalRoute.getRoute() + nestedRoute.key.route;
 				this.register(nestedRoute.value, nestedRoute.key);
 			}
@@ -73,14 +87,45 @@ export default class Router {
 		this.routes.push(internalRoute);
 	}
 
-	private getRouteConfig(route:NewableRoute) : RouteConfig {
+	public registerCallback(type: RouterCallbackType, callback: Function) {
+		this.findCallbackCollection(type).push(callback);
+	}
+
+	private findRoute(packet:SocketIOExt.Packet) {
+		for (const route of this.routes) {
+			if (route.getRoute() === packet.data[0]) {
+				return route;
+			}
+		}
+
+		return null;
+	}
+
+	private runCallbacks(type: RouterCallbackType, ...args) {
+		this.findCallbackCollection(type).forEach(fn => fn(args));
+	}
+
+	private findCallbackCollection(type: RouterCallbackType) {
+		switch (type) {
+			case RouterCallbackType.BEFORE_EVENT:
+				return this.beforeCallbacks;
+			case RouterCallbackType.ON_EVENT:
+				return this.onCallbacks;
+			case RouterCallbackType.AFTER_EVENT:
+				return this.afterCallbacks;
+
+			default:
+				throw new Error(`Found no fitting callback collection for callback type: ${type}`);
+		}
+	}
+
+	private getRouteConfig(route: NewableRoute): RouteConfig {
 		return RouteDecorator.getRouteMetadata(route);
 	}
 
-	private getNestedRouteConfig(route:NewableRoute, property:string) : RouteConfig {
+	private getNestedRouteConfig(route: NewableRoute, property: string): RouteConfig {
 		return RouteDecorator.getNestedRouteMetadata(route, property);
 	}
-
 
 	private invokeRoute(route: InternalRoute, socket: SocketIOExt.Socket) {
 		const instance = route.getInstance();
--- src/structures/Pair.ts
+++ src/structures/Pair.ts
@@ -2,7 +2,7 @@ export class TypedPair<K, V> {
 	public key:K;
 	public value:V;
 
-	constructor(key:K, value:V) {
+	public constructor(key:K, value:V) {
 		this.key = key;
 		this.value = value;
 	}

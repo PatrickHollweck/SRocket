693e790d8635a37911266f152da7e2e0bcd65e46
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-03-20 18:29:01 +0100

    Added helper methods to repl

--- helpers/socket-client-REPL.js
+++ helpers/socket-client-REPL.js
@@ -2,3 +2,11 @@ var repl = require("repl");
 var r = repl.start("srocket> ");
 
 r.context.s = require("socket.io-client")("http://localhost:1340");
+
+r.context.emitParam = function() {
+	r.context.s.emit("/param", { userName: "patrick" });	
+}
+
+r.context.emitModel = function() {
+	r.context.s.emit("/model", { userName: "Patrick" });
+}

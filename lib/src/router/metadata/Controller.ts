b06eafbca51f3ac075cb3b2c9be9e09baf61e609
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-08-03 15:42:04 +0200

    Removed duplicate "Controller" definition

--- lib/src/router/metadata/Controller.ts
+++
@@ -1,6 +0,0 @@
-import { RouteReturn } from "../Route";
-
-export abstract class Controller {
-	$onConnect?(socket: SocketIO.Socket): RouteReturn;
-	$onDisconnect?(socket: SocketIO.Socket): RouteReturn;
-}

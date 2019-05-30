6139df0d74f48e4bf90bdbda79ce8cb078251d4d
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-05-30 13:52:03 +0200

    Update addons deploy

--- scripts/deploy.addons.js
+++ scripts/deploy.addons.js
@@ -1,4 +1,14 @@
+const reader = require("readline-sync");
+const fs = require("fs");
+
+const packagePath = reader.question("Which addon do you want to publish? - Path from addons folder: ");
+
+if (!fs.existsSync("./addons/" + packagePath)) {
+	console.log("Invalid Package path!");
+	process.exit(1);
+}
+
 require("./deploy.base")(
-	"./addons/middleware-validation-joi/",
-	"./addons/middleware-validation-joi/package.json"
+	"./addons/" + packagePath,
+	"./addons/" + packagePath + "/package.json"
 );
